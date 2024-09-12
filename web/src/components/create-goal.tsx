43d415685411b7196b4704299193de0adf254e86
julialealt <julialeal.t@gmail.com>
2024-09-12 13:32:06 -0300

    final commit

---
+++ web/src/components/create-goal.tsx
@@ -0,0 +1,180 @@
+import { X } from 'lucide-react'
+import lazyEmoji from '../assets/lazy-emoji.svg'
+import smileEmoji from '../assets/smile-emoji.svg'
+import coolEmoji from '../assets/cool-emoji.svg'
+import crazyEmoji from '../assets/crazy-emoji.svg'
+import starEyesEmoji from '../assets/star-eyes-emoji.svg'
+import explodingHeadEmoji from '../assets/exploding-head-emoji.svg'
+import fireEmoji from '../assets/fire-emoji.svg'
+import { Button } from './ui/button'
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from './ui/dialog'
+import { Label } from './ui/label'
+import { Input } from './ui/input'
+import {
+  RadioGroup,
+  RadioGroupIndicator,
+  RadioGroupItem,
+} from './ui/radio-group'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createGoal } from '../http/create-goal'
+import { useQueryClient } from '@tanstack/react-query'
+
+const createGoalForm = z.object({
+  title: z.string().min(1, 'Informe a atividade que deseja realizar'),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+})
+
+type CreateGoalForm = z.infer<typeof createGoalForm>
+
+export function CreateGoal() {
+  const queryClient = useQueryClient()
+
+  const { register, control, handleSubmit, formState, reset } =
+    useForm<CreateGoalForm>({
+      resolver: zodResolver(createGoalForm),
+    })
+
+  async function handleCreateGoal(data: CreateGoalForm) {
+    await createGoal({
+      title: data.title,
+      desiredWeeklyFrequency: data.desiredWeeklyFrequency,
+    })
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+
+    reset()
+  }
+
+  return (
+    <DialogContent>
+      <div className="flex flex-col gap-6 h-full">
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-between">
+            <DialogTitle>Cadastrar meta</DialogTitle>
+            <DialogClose>
+              <X className="size-5 text-zinc-600" />
+            </DialogClose>
+          </div>
+
+          <DialogDescription>
+            Adicione atividades que te fazem bem e que você quer continuar
+            praticando toda semana.
+          </DialogDescription>
+        </div>
+
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex-1 flex flex-col justify-between"
+        >
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Qual a atividade?</Label>
+              <Input
+                id="title"
+                autoFocus
+                placeholder="Praticar exercícios, meditar, ..."
+                {...register('title')}
+              />
+
+              {formState.errors.title && (
+                <p className="text-red-400 text-sm">
+                  {formState.errors.title.message}
+                </p>
+              )}
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <Label>Quantas vezes na semana?</Label>
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                defaultValue={1}
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                    >
+                      <RadioGroupItem value="1">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          1x na semana
+                        </span>
+                        <img src={lazyEmoji} alt="in.orbit" />
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="2">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          2x na semana
+                        </span>
+                        <img src={smileEmoji} alt="in.orbit" />
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="3">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          3x na semana
+                        </span>
+                        <img src={coolEmoji} alt="in.orbit" />
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="4">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          4x na semana
+                        </span>
+                        <img src={crazyEmoji} alt="in.orbit" />
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="5">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          5x na semana
+                        </span>
+                        <img src={starEyesEmoji} alt="in.orbit" />
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="6">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          6x na semana
+                        </span>
+                        <img src={explodingHeadEmoji} alt="in.orbit" />
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="7">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          Todos os dias da semana
+                        </span>
+                        <img src={fireEmoji} alt="in.orbit" />
+                      </RadioGroupItem>
+                    </RadioGroup>
+                  )
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <DialogClose asChild>
+              <Button className="flex-1" variant="secondary">
+                Fechar
+              </Button>
+            </DialogClose>
+            <Button className="flex-1">Salvar</Button>
+          </div>
+        </form>
+      </div>
+    </DialogContent>
+  )
+}
